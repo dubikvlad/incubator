b62f178e59fc6763c15ce1cbe8269b8316e0acf0
Vlad Dubovski <dubikvlad@mail.ru>
2021-03-12 16:18:28 +0300

    added fix NOK

--- Base4/2/1.cpp
+++ Base4/2/1.cpp
@@ -1,6 +1,5 @@
-/*
-    1. НОК. Найдите наименьшее общее кратное двух натуральных чисел.
-*/
+//    1. НОК. Найдите наименьшее общее кратное двух натуральных чисел.
+
 
 #include<iostream>
 using namespace std;
@@ -11,18 +10,31 @@ int main()
 
     cout << "Enter first number ";
     cin >> a;
-
     cout << "Enter second number ";
     cin >> b;
 
-    for(int i = a; i > 0; i--){
-        if (a % i == 0 && b % i == 0) {
-            NOD = i;
-            break;
-        }
+    while(a < 0 or b < 0){
+        cout << "Error! Numbers couldn't be less than 0 \n";
+        cout << "Enter first number ";
+        cin >> a;
+        cout << "Enter second number ";
+        cin >> b;
     }
-
-    NOK = a * b / NOD;
+    if(a == 0){
+        NOK = b;
+    }
+	else if(b == 0){
+		NOK = a;
+    }    
+    else{
+        for(int i = a; i > 0; i--){
+            if (a % i == 0 && b % i == 0) {
+                NOD = i;
+                break;                
+            }
+        }
+        NOK = a * b / NOD;
+    }       
 
     cout << "NOK numbers " << a << " and " << b << " = " << NOK;
 
